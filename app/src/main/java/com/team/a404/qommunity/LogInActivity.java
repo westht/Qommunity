0a7af4b3f271ccae36543e291bfb1a578df0a322
Sergio Cuadrado <sergiocv97@gmail.com>
2018-01-15 18:41:15 +0100

    Commit Inicial

---
+++ app/src/main/java/com/team/a404/qommunity/LogInActivity.java
@@ -0,0 +1,13 @@
+package com.team.a404.qommunity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+    }
+}
