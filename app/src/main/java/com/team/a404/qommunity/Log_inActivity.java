421807ce3756574382121ab1bc25b31e5fa97f40
Sergio Cuadrado <sergiocv97@gmail.com>
2018-01-22 13:50:56 +0100

    Commit 3-Cambio de Pantallas Main/Login/SignUp

---
+++ app/src/main/java/com/team/a404/qommunity/Log_inActivity.java
@@ -0,0 +1,36 @@
+package com.team.a404.qommunity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Sergio Cuadrado on 22/01/2018.
+ */
+
+public class Log_inActivity extends AppCompatActivity {
+    protected EditText user;
+    protected EditText pass;
+    protected TextView sincuenta;
+    protected Button log;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activ);
+        user = (EditText)findViewById(R.id.usuario);
+        pass=(EditText)findViewById(R.id.pass);
+        sincuenta = (TextView)findViewById(R.id.nocuenta);
+        log = (Button)findViewById(R.id.logbutt);
+        sincuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Log_inActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/team/a404/qommunity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.team.a404.qommunity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        Intent intent = new Intent(this, Log_inActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+
+    }
+}
