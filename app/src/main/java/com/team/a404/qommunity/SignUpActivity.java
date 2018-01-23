daefb988784081dc62f154d7dc07ed04c693d5da
borja <borja@borja.com>
2018-01-23 20:11:39 +0100

    Diseño de registro

--- app/src/main/java/com/team/a404/qommunity/SignUpActivity.java
+++ app/src/main/java/com/team/a404/qommunity/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.team.a404.qommunity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by Sergio Cuadrado on 22/01/2018.
@@ -11,6 +14,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.sign_up);
     }
 }
