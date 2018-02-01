094175e1953b7892e69598606fd58b9e8024ccf8
Sergio Cuadrado <sergiocv97@gmail.com>
2018-02-01 18:31:22 +0100

    Commit - Pantalla de Ajustes y inserción de datos

--- app/src/main/java/com/team/a404/qommunity/MainScreen.java
+++ app/src/main/java/com/team/a404/qommunity/MainScreen.java
@@ -5,20 +5,32 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 /**
  * Created by Sergio Cuadrado on 25/01/2018.
  */
+
+
 
 public class MainScreen extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
     private Button boton_logout;
     private ImageButton settings;
+    private DatabaseReference DataRef;
 
+    private EditText et_name;
+    private EditText et_adres;
+    private Button btn_enviar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +44,12 @@ public class MainScreen extends AppCompatActivity implements View.OnClickListene
             }
         });
 
+        et_name = (EditText) findViewById(R.id.et_name);
+        et_adres = (EditText) findViewById(R.id.et_address);
+        btn_enviar = (Button) findViewById(R.id.btn_enviar);
+
         firebaseAuth = FirebaseAuth.getInstance();
+        DataRef = FirebaseDatabase.getInstance().getReference();
 
         if (firebaseAuth.getCurrentUser() == null) {
             finish();
@@ -42,9 +59,21 @@ public class MainScreen extends AppCompatActivity implements View.OnClickListene
         boton_logout = (Button) findViewById(R.id.boton_logout);
 
         boton_logout.setOnClickListener(this);
-        
+        btn_enviar.setOnClickListener(this);
     }
 
+    private void SalvarDatos(){
+        String name = et_name.getText().toString().trim();
+        String address = et_adres.getText().toString().trim();
+
+        UserInformation user = new UserInformation(name,address);
+
+        FirebaseUser usuario = firebaseAuth.getCurrentUser();
+
+        DataRef.child(usuario.getUid()).setValue(user);
+
+        Toast.makeText(this, "Informacion guardada",Toast.LENGTH_LONG).show();
+    }
     @Override
     public void onClick(View view) {
         if (view == boton_logout) {
@@ -52,6 +81,9 @@ public class MainScreen extends AppCompatActivity implements View.OnClickListene
             firebaseAuth.signOut();
             Intent intent = new Intent(MainScreen.this, Log_inActivity.class);
             startActivity(intent);
+        }
+        if (view == btn_enviar){
+            SalvarDatos();
         }
     }
 }
---
+++ app/src/main/java/com/team/a404/qommunity/UserInformation.java
@@ -0,0 +1,19 @@
+package com.team.a404.qommunity;
+
+/**
+ * Created by Sergio Cuadrado on 01/02/2018.
+ */
+
+public class UserInformation {
+    public String name;
+    public String address;
+
+    public UserInformation(){
+
+    }
+
+    public UserInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
