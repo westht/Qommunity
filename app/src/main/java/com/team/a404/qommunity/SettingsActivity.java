33d5a72b0a4baaa3aa92bcebee9048c69dea8499
Sergio Cuadrado <sergiocv97@gmail.com>
2018-02-08 17:09:59 +0100

    Commmit - Panel desplegable 2-5

--- app/src/main/java/com/team/a404/qommunity/SettingsActivity.java
+++ app/src/main/java/com/team/a404/qommunity/SettingsActivity.java
@@ -4,7 +4,9 @@ package com.team.a404.qommunity;
  * Created by Sergio Cuadrado on 01/02/2018.
  */
 
+import android.content.Intent;
 import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
 
@@ -17,7 +19,15 @@ public class SettingsActivity extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
-
+        completa = (EditTextPreference)findPreference("edit_text_preference_1");
+        completa.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(SettingsActivity.this,CompletaPerfil.class);
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 
 }
