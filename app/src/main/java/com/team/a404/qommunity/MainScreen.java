3a2601764d717be2a8bcf816cfdbfe75227834f5
Borja <borjalagarejo@gmail.com>
2018-02-13 00:43:05 +0100

    Actualizacion OK

--- app/src/main/java/com/team/a404/qommunity/MainScreen.java
+++ app/src/main/java/com/team/a404/qommunity/MainScreen.java
@@ -1,9 +1,14 @@
 package com.team.a404.qommunity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
