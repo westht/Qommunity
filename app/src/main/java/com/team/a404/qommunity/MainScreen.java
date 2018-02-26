7aa55d902013ea24e6ea2af362815d3b161d4b75
Sergio Cuadrado <sergiocv97@gmail.com>
2018-02-26 18:37:55 +0100

    Commit Cosas con Imagenes

--- app/src/main/java/com/team/a404/qommunity/MainScreen.java
+++ app/src/main/java/com/team/a404/qommunity/MainScreen.java
@@ -80,6 +80,7 @@ public class MainScreen extends AppCompatActivity
         startActivity(intenti);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
