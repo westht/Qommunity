2296148624eafd130e3caf658fd8cd878d602495
Borja <borjalagarejo@gmail.com>
2018-02-27 15:06:15 +0100

    Add favores

---
+++ app/src/main/java/com/team/a404/qommunity/Favores/ListFavores_add.java
@@ -0,0 +1,28 @@
+package com.team.a404.qommunity.Favores;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+import com.team.a404.qommunity.R;
+
+public class ListFavores_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_favores_add);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setIcon(R.drawable.ic_arrow_down);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_listfavores_add,menu);
+        return true;
+    }
+
+}
